8f4d84d64ae794c94d4e81bdad597860a9550e95
unknown <aghazjunaid96@gmail.com>
2022-04-22 17:20:46 +0530

    top offers page

--- src/data.js
+++ src/data.js
@@ -184,3 +184,138 @@ export const dealsOfTheDayData = [
         description: "Wedding Special Jewellery!"
     },
 ]
+
+export const faishonTopDealsData = [
+    {
+        url: "/assets/images/m-usss0739-u-s-polo-association-original-imafhgsx2wjewveh.jpeg",
+        title: "Flying Machine, Mufti, Puma..",
+        price: "50-80%+ Extra 10% Off",
+        description: "T-Shirts, Jeans, Sweatshirts..."
+    },
+    {
+        url: "/assets/images/m-usts5923-u-s-polo-association-original-imaf9hu5xscbhrz3.jpeg",
+        title: "U.S.P.A, Levi's, Puma...",
+        price: "Under ₹999",
+        description: "Premium Brands On Sale"
+    },
+    {
+        url: "/assets/images/32-maw19jn132b-metronaut-original-imafhhushjecudhs.jpeg",
+        title: "Wrangler, Killer, Arrow...",
+        price: "Min 50%+Extra 10% off",
+        description: "Jeans, Chinos, Trousers.."
+    },
+    {
+        url: "/assets/images/m-7817-maroon-libas-original-imafjd7tvtpmgfcz.jpeg",
+        title: "Kurtas & kurtis",
+        price: "Min 50% off",
+        description: "A-Line, Anarkali & more"
+    },
+    {
+        url: "/assets/images/crone-backpack-08-teal-fg8-0-11-208-backpack-american-tourister-original-imafh54hjfhafh6v.jpeg",
+        title: "American Tourister, Skybags...",
+        price: "Min 50% Off",
+        description: "Backpacks"
+    },
+    {
+        url: "/assets/images/f52d84022612187d.jpg",
+        title: "Casio, fastrack & more",
+        price: "20 - 60% Off",
+        description: "Men & Women Watches"
+    },
+    {
+        url: "/assets/images/fsnk107nt-fsnnv001hn-hand-held-bag-speed-x-fashion-original-imafgpzjp9hsfeh3.jpeg",
+        title: "Lavie, Caprese, Baggit...",
+        price: "40-70% Off",
+        description: "Handbags"
+    },
+]
+
+export const bigStealsOfTheWeekData = [
+    {
+        url: "/assets/images/ka-ht-214-craftsfort-original-imag5qwapevkuxuu.jpeg",
+        title: "Luggage Rack, Shoe Racks ....",
+        price: "Upto 65% off",
+        description: "Solidwood,Metal,Plastic,Kids ..."
+    },
+    {
+        url: "/assets/images/gc-1120111cma-45-woodland-denim-original-imafdrgtvvmmdveg.jpeg",
+        title: "Woodland, Lee Cooper & more",
+        price: "Up to 60% off",
+        description: "Men's Casual Shoes, Sandals..."
+    },
+    {
+        url: "/assets/images/samsung-sm-t975nzkainu-original-imafuhg9tfydgg78.jpeg",
+        title: "SAMSUNG Galaxy Tab S7+ 6 GB RAM",
+        price: "Extra ₹2000 Off!",
+        description: "Electronics, Mobile..."
+    },
+    {
+        url: "/assets/images/s704y-10-asics-grand-shark-white-original-imafuxxxgxgz8d4r.jpeg",
+        title: "ADIDAS, ASICS, Puma...",
+        price: "Upto 60%+Extra 5% Off",
+        description: "Men's Footwear"
+    },
+    {
+        url: "/assets/images/606678c64c32301a.jpg",
+        title: "Gold Coins & Precious Jewellery",
+        price: "Upto 20% Off",
+        description: "Malabar Gold, Candere..."
+    },
+    {
+        url: "/assets/images/-original-imagahnnunvh24f2.jpeg",
+        title: "ZEBRONICS Smartwatch",
+        price: "Upto 50% off",
+        description: "From ₹1999"
+    },
+    {
+        url: "/assets/images/8f8035fcd64a2c12.jpg",
+        title: "Belts & Wallets",
+        price: "From ₹299",
+        description: "#Grab Or Gone"
+    },
+]
+
+export const topOffersData = [
+    {
+        url: "/assets/images/m-151tkr2811-selvia-original-imagyfmzv4yhgxxf.jpeg",
+        title: "Dresses, T-Shirts, Tops...",
+        price: "Upto 60% Off",
+        description: "Sassafras, Nike, Vero Moda"
+    },
+    {
+        url: "/assets/images/no-regular-arg-b39-yes-android-ios-honor-yes-original-imag3ru49v8bsqh7.jpeg",
+        title: "Honor Band 6",
+        price: "Flat 50% off",
+        description: "Just ₹2999"
+    },
+    {
+        url: "/assets/images/42-14381888-mast-harbour-original-imag88krppztwqhg.jpeg",
+        title: "Arrow, UCB, Levi's..",
+        price: "Up to 60%+Extra10%Off",
+        description: "Shirts, Trousers.."
+    },
+    {
+        url: "/assets/images/pluto-bajaj-original-imag8yqnfkzzbpkg.jpeg",
+        title: "Mixers, Induction Cooktops ...",
+        price: "Up to 55% Off",
+        description: "Preethi, Philips & more"
+    },
+    {
+        url: "/assets/images/400-hamilton-deodorant-body-spray-denver-men-original-imafgb9fsafampwz.jpeg",
+        title: "Deodorants",
+        price: "Min 30% Off",
+        description: "Grab Now!"
+    },
+    {
+        url: "/assets/images/200-gm-strawberry-powder-natural-spry-dried-1-bliss-of-earth-original-imag78yykwvyhfwh.jpeg",
+        title: "Instant Drink Mix",
+        price: "Min 20% Off",
+        description: "Grab Now!"
+    },
+    {
+        url: "/assets/images/med-blk-c-medellin-original-imaf22pgyd8w5krp.jpeg",
+        title: "Acoustic Guitars",
+        price: "Starting ₹2199",
+        description: "Medellin, Juarez & More"
+    },
+]
--- src/pages/Navdata-page.js
+++ src/pages/Navdata-page.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { navDatas,dealsOfTheDayData } from "../data";
+import { navDatas,dealsOfTheDayData,faishonTopDealsData, bigStealsOfTheWeekData,topOffersData } from "../data";
 import Product from "../components/Product";
 
 const NavDataPage = () => {
@@ -21,10 +21,10 @@ const NavDataPage = () => {
                     }
                 </div>
             </div>
-            <div class="deals">
-                <div class="footwear" style={{paddingTop: "15px"}}>
-                    <div class="footwear-header">
-                        <div class="footwear-header-clock">
+            <div className="deals">
+                <div className="footwear" style={{paddingTop: "15px"}}>
+                    <div className="footwear-header">
+                        <div className="footwear-header-clock">
                             <h2 style={{
                                         fontSize: "22px",
                                         lineHeight: "32px",
@@ -32,7 +32,7 @@ const NavDataPage = () => {
                                         fontWeight: "500"
                                         }}>Deals of the Day
                             </h2>
-                            <div class="clock">
+                            <div className="clock">
                                 <img width="20" src="https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/timer_a73398.svg" alt="clock"/>
                                 <span>21 : 08 : 47 Left</span>
                             </div>
@@ -40,7 +40,7 @@ const NavDataPage = () => {
                         <input type="button" value="VIEW ALL"/>
                     </div>
                     <hr/>
-                    <div class="footwear-cards">
+                    <div className="footwear-cards">
                         {
                             dealsOfTheDayData.map((ele,index) => {
                                 return(
@@ -49,6 +49,56 @@ const NavDataPage = () => {
                             })
                         }
                     </div>
+                </div>
+            </div>
+            <div className="footwear">
+                <div className="footwear-header">
+                    <h2>Fashion Top Deals</h2>
+                    <input type="button" value="VIEW ALL"/>
+                </div>
+                <hr/>
+                <div className="footwear-cards">
+                {
+                    faishonTopDealsData.map((ele,index) => {
+                        return(
+                            <Product ele={ele} index={index} />
+                        )
+                    })
+                }
+                </div>
+            </div>
+
+            <div className="footwear">
+                <div className="footwear-header">
+                    <h2>Big Steals of the Week</h2>
+                    <input type="button" value="VIEW ALL"/>
+                </div>
+                <hr/>
+                <div className="footwear-cards">
+                {
+                    bigStealsOfTheWeekData.map((ele,index) => {
+                        return(
+                            <Product ele={ele} index={index} />
+                        )
+                    })
+                }
+                </div>
+            </div>
+
+            <div className="footwear">
+                <div className="footwear-header">
+                    <h2>Top Offers</h2>
+                    <input type="button" value="VIEW ALL"/>
+                </div>
+                <hr/>
+                <div className="footwear-cards">
+                {
+                    topOffersData.map((ele,index) => {
+                        return(
+                            <Product ele={ele} index={index} />
+                        )
+                    })
+                }
                 </div>
             </div>
         </>
